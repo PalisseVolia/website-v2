128b30162f0edd1ae6652e44bba806f76679fcff
PalisseVolia <palissev@gmail.com>
2023-02-23 16:13:03 +0100

    vite change

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { useParallax } from 'react-scroll-parallax';
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default App
