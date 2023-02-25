e7dd386c7eae1b4792448bfb283f08fdc2309396
PalisseVolia <palissev@gmail.com>
2023-02-25 21:42:23 +0100

    aa

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,13 +1,43 @@
-import { useState } from 'react'
-import { useParallax } from 'react-scroll-parallax';
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import { useEffect, useRef } from 'react';
+import RotatingLine from "./components/RotatingLine";
+import './App.css';
+
+import Nasa from "./img/nasa.jpg";
 
 function App() {
     return (
-        <div>
-
-        </div>
+        <>
+            <div className="NavBar">
+                <button className="NavButtons">BUTTON1</button>
+                <button className="NavButtons">BUTTON2</button>
+                <button className="NavButtons">BUTTON3</button>
+                <button className="NavButtons">BUTTON4</button>
+            </div>
+            <div>
+                <Parallax pages={4}>
+                    <ParallaxLayer id="temp">
+                        <div id="Header">
+                            <h1 className="HeaderTitle">Palisse</h1>
+                            <h1 className="HeaderTitle">Volia</h1>
+                            <div className="HeaderSubTitle">
+                                <p>Electrical engineering student  </p>
+                                <RotatingLine />
+                            </div>
+                        </div>
+                    </ParallaxLayer>
+                    <ParallaxLayer
+                        offset={2}
+                        speed={1}
+                        factor={1}
+                        style={{
+                            backgroundImage: `url(${Nasa})`,
+                            backgroundSize: 'cover',
+                        }}>
+                    </ParallaxLayer>
+                </Parallax>
+            </div>
+        </>
     )
 }
 
---
+++ portfolio/src/components/RotatingLine.jsx
@@ -0,0 +1,9 @@
+export default function RotatingLine() {
+    return (
+        <>
+            <p id="a1">--</p>
+            <p id="a2">/</p>
+            <p id="a3">\</p>
+        </>
+    );
+}
