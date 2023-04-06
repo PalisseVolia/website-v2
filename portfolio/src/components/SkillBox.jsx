553b86c871d1b6d02656dd6444371b4c7b5bb68b
PalisseVolia <palissev@gmail.com>
2023-04-07 01:57:52 +0200

    maj

--- portfolio/src/components/SkillBox.jsx
+++ portfolio/src/components/SkillBox.jsx
@@ -1,8 +1,13 @@
+import dlpng from '../img/downloadpdf.png';
+
 export default function SkillBox() {
     return (
         <div className="SkillBox">
             <div className="SBtop">
                 <p className="Intitule">Intitule</p>
+                <div>
+                    <img src={dlpng} alt="Download PDF" />
+                </div>
             </div>
             <div className="SBmid">
                 <p className="Nom">Nom</p>
